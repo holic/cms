7a078030105276da75182fbcf5cbc6f7699ba9cf
Kevin Ingersoll <kingersoll@gmail.com>
2015-04-07 11:34:00 +1200

    Add author model and fixtures

---
+++ src/models/author.js
@@ -0,0 +1,21 @@
+module.exports = {
+	label: 'Author',
+	property: 'authors',
+	type: 'collection',
+	fields: [
+		{
+			label: 'Name',
+			property: 'name',
+			type: 'text',
+			required: true,
+			listed: true
+		},
+		{
+			label: 'Twitter',
+			property: 'twitter',
+			type: 'text',
+			required: true,
+			listed: true
+		}
+	]
+}
