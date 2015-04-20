4a955f770d20376e92ba05031137d6728445e7f3
Kevin Ingersoll <kingersoll@gmail.com>
2015-04-20 11:45:01 -0700

    Load correct fixtures when navigating

--- src/components/entries/index.js
+++ src/components/entries/index.js
@@ -1,17 +1,35 @@
+var fixtures = require('../../fixtures')
+
 module.exports = {
 	inherit: true,
 	template: require('./entries.html'),
+	methods: {
+		activateModel: function (active) {
+			var activeModel
+			this.models.forEach(function (model) {
+				if (model.property === active) {
+					activeModel = model
+				}
+			})
+
+			this.model = activeModel
+			this.entries = fixtures[active]
+		}
+	},
 	data: function () {
-		var active = this.activeModel
-		var activeModel
-		this.models.forEach(function (model) {
-			if (model.property === active) {
-				activeModel = model
-			}
-		})
 		return {
-			model: activeModel,
-			entries: require('../../fixtures/posts.json')
+			model: null,
+			entries: null
+		}
+	},
+	created: function () {
+		if (this.activeModel) {
+			this.activateModel(this.activeModel)
+		}
+	},
+	watch: {
+		activeModel: function (model) {
+			this.activateModel(model)
 		}
 	}
 }
---
+++ src/fixtures/index.js
@@ -0,0 +1,4 @@
+module.exports = {
+	authors: require('./authors.json'),
+	posts: require('./posts.json')
+}
