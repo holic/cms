594657a65b13d7069458ea66b361891ed9fb817f
Kevin Ingersoll <kingersoll@gmail.com>
2015-05-01 17:16:19 -0700

    Show uploading indicator

--- src/components/fields/image/index.js
+++ src/components/fields/image/index.js
@@ -3,6 +3,11 @@ var valueToProperty = require('../valueToProperty')
 module.exports = {
 	mixins: [valueToProperty],
 	template: require('./image.html'),
+	data: function () {
+		return {
+			uploading: false
+		}
+	},
 	computed: {
 		thumbnail: function () {
 			return this.value + '-/resize/300/'
@@ -17,11 +22,17 @@ module.exports = {
 			uploadcare.openDialog(null, {
 				crop: 'disabled',
 				imagesOnly: true
-			}).done(function (file) {
-				console.log('file:', file)
-				file.promise().done(function (fileInfo) {
-					console.log('file data:', fileInfo)
+			})
+			.done(function (file) {
+				console.log('Uploading file:', file)
+				vm.uploading = true
 
+				file.promise()
+				.always(function () {
+					vm.uploading = false
+				})
+				.done(function (fileInfo) {
+					console.log('Uploaded file data:', fileInfo)
 					vm.value = fileInfo.originalUrl
 				})
 			})
