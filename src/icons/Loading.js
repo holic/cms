e2d9c0375892be2bf9d50a7c0b6dffeab79ce15b
Kevin Ingersoll <kingersoll@gmail.com>
2016-05-21 22:47:45 -0700

    Give names to stateless components

--- src/icons/Loading.js
+++ src/icons/Loading.js
@@ -2,7 +2,7 @@ import React from 'react'
 import ReloadIcon from './Reload'
 import classNames from 'classnames'
 
-export default function (props) {
+export default function LoadingIcon (props) {
   return (
     <ReloadIcon {...props} className={classNames(props.className, 'icon-spin text-info')} />
   )
