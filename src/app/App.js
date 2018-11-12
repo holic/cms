82a08bb5eabdd3a11edc9091eb4d75347a380e13
Kevin Ingersoll <kingersoll@gmail.com>
2018-11-11 19:20:04 -0800

    Remove no longer used Firebase component and react-firebase lib

--- src/app/App.js
+++ src/app/App.js
@@ -1,20 +1,16 @@
 import React from "react";
 import { BrowserRouter, Route } from "react-router-dom";
-import { Provider } from "react-firebase";
-import { app as firebaseApp } from "../firebase";
 import Layout from "./Layout";
 
 const App = () => (
-  <Provider firebaseApp={firebaseApp}>
-    <BrowserRouter>
-      {/*
+  <BrowserRouter>
+    {/*
         We're using a Route with render prop to render the Layout instead of
         rendering it directly so that we can pass in a location and break
         memoization of the Layout component.
       */}
-      <Route render={({ location }) => <Layout location={location} />} />
-    </BrowserRouter>
-  </Provider>
+    <Route render={({ location }) => <Layout location={location} />} />
+  </BrowserRouter>
 );
 
 export default React.memo(App);
